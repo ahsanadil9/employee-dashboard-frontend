12be509b0183202efe535098f2c661a54252266c
Ahsan Adil <muhammadmahsanadil@gmail.com>
2024-11-12 02:11:30 +0500

    remote-fronend-work

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path if necessary
+//   ],
+//   // content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   // style: {
+//   //   postcssOptions: {
+//   //     plugins: [require("tailwindcss"), require("autoprefixer")],
+//   //   },
+//   // },
+// };
+
+// // module.exports = {
+// //   plugins: {
+// //     tailwindcss: {},
+// //     autoprefixer: {},
+// //   },
+// // }
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import EmployeeDashboard from "./employee-dashboard";
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <EmployeeDashboard />
+      {/* hello */}
+    </div>
+  );
+}
+
+export default App;
