a7a6ca4a96cdc663f30978bc4e34df0a6e4428ec
Ahsan Adil <muhammadmahsanadil@gmail.com>
2024-11-12 22:29:46 +0500

    performance optimize

--- src/components/common/menu/hamburger-menu.tsx
+++ src/components/common/menu/hamburger-menu.tsx
@@ -16,7 +16,7 @@ export const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
 }) => (
   <div className="flex justify-between items-center py-4 px-6 md:px-4 font-bold fixed top-0 bg-white w-full shadow-md z-20">
     <div className="flex items-center text-blue-600 cursor-pointer">
-      Employee Dashboard
+      Employee's Dashboard
     </div>
 
     <div className="md:hidden">
